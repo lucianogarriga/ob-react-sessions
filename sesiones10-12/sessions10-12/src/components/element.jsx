fe65801671ab30d398af62a1f48bd14727336a2f
Luciano Garriga <lucianomgarriga@gmail.com>
2023-07-10 11:38:05 -0300

    Add cursor style and clean code

--- sesiones10-12/sessions10-12/src/components/element.jsx
+++ sesiones10-12/sessions10-12/src/components/element.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 
 const Element = () => {
-  
   // State and setState function w/ initialValue "black"
-  const [color, setColor] = useState("black")
-  const intervalRef = useRef(null)
+  const [color, setColor] = useState("black");
+  const intervalRef = useRef(null);
   // Variable with bg color => state component (black)
   const squareStyle = {
-    backgroundColor: color
-  }
+    backgroundColor: color,
+    cursor: "pointer"
+  };
 
   // Function to generate random colors
   const generateRandomColor = () => {
@@ -17,31 +17,35 @@ const Element = () => {
     const blue = Math.floor(Math.random() * 256);
     const rgb = `rgb(${red},${green},${blue})`;
     setColor(rgb);
-  }
+  };
 
   // Handler Events
   const onMouseEnterHandler = () => {
     intervalRef.current = setInterval(generateRandomColor, 500);
-  } 
+  };
   const onMouseLeaveHandler = () => {
-    clearInterval(intervalRef.current)
-  } 
+    clearInterval(intervalRef.current);
+  };
 
   //
-  useEffect(() => { 
+  useEffect(() => {
     // The Interval will clean when the component will unmount
-    return clearInterval(intervalRef.current)
+    return clearInterval(intervalRef.current);
     // [] => the function will execute 1 time, when the component will unmount
   }, []);
 
-
   return (
     <>
-     <div className="elemento" style={squareStyle} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} onDoubleClick={onMouseLeaveHandler}>
-        <h1></h1>
-     </div> 
-    </> 
-  )
+      <div
+        className="elemento"
+        style={squareStyle}
+        onMouseEnter={onMouseEnterHandler}
+        onMouseLeave={onMouseLeaveHandler}
+        onDoubleClick={onMouseLeaveHandler}
+      > 
+      </div>
+    </>
+  );
 };
 
 export default Element;
